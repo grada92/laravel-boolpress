dd2138d40248fb7eaf3231a62a847dc16e73683e
grada92 <danielegradassai@gmail.com>
2022-11-18 18:26:27 +0100

    Project Many-to-Many Completed

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <a href="{{route('admin.categories.create')}}">Create new category</a>
 @foreach ($categories as $category)
-
+                            <!--Guardare le rotte da terminale admin/categories/{category}-->
 <div>{{$category->name}}</div>    <!--Prendo anche id che distingue la categoria da prendere-->
 <form action="{{ route('admin.categories.destroy', $category->id)}}" method="POST">
     @csrf
